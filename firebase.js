fcae92f8dff1da0fd31de50eb01c24719c059edf
NehanHettiarach <6083582-NehanHettiarach@users.noreply.replit.com>
2023-04-30 08:28:21 +0000

    V2: Beginning implementation of login and registration
    V2: HTML CSS, with non functioning login button

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Firebase Config
+var firebaseConfig = {
+  apiKey: "AIzaSyCs10NnQ4BIdtlDxmGMoLJgZ-OkNIu6VFM",
+  authDomain: "comp-nehan-hettiarachchi.firebaseapp.com",
+  databaseURL: "https://comp-nehan-hettiarachchi-default-rtdb.firebaseio.com",
+  projectId: "comp-nehan-hettiarachchi",
+  storageBucket: "comp-nehan-hettiarachchi.appspot.com",
+  messagingSenderId: "72222490884",
+  appId: "1:72222490884:web:2446be6e4af2a954fa5051",
+  measurementId: "G-8MEZ1VQRTY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+database = firebase.database();
+
+/*****************************************************/
+//    END OF MODULE
+/*****************************************************/
